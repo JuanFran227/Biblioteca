c6e276e4b58980fb271f3107111da17ecc83c604
jfbarrenag01 <jfbarrenag01@iesalbarregas.es>
2026-02-04 20:09:19 +0100

    ·commit con añadidos

--- Biblioteca/src/Libros.java
+++ Biblioteca/src/Libros.java
@@ -5,6 +5,20 @@ public class Libros {
     ○ Buscar libros por título, autor o categoría.(todos)
     ○ Mostrar todos los libros disponibles.(todos)*/
 
-    
+    String titulo;
+    String autor;
+    String categoría;
+    int cantidad;
+
+    public void Libros(){
+
+    }
+
+    public void Libros(String titulo, String autor, String categoría, int cantidad){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.categoría = categoría;
+        this.cantidad = cantidad;
+    }
     
 }
